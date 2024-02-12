c6bd0167cc454ea249c7d22286f41a605650cbbd
Charles Bedell <charlesbedell5@gmail.com>
2024-02-12 15:51:16 -0500

    custom colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: ["./src/**/*.jsx"],
   theme: {
+    colors: {'blue': '#0b4cff',
+             'black': '#030303',
+             'dark-grey': '#1e1f26',
+             'grey': '#858585',
+             'light-grey': '#eeeeee',
+             'white': '#ffffff',
+             'pink': '#de63e7',             
+             },
     extend: {},
   },
   plugins: [],
-}
-
+};
