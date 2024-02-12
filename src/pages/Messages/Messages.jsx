2869c3ce11f4fd3a1322449c9f407c2ecc152811
Charles Bedell <charlesbedell5@gmail.com>
2024-02-12 11:20:01 -0500

    make navbar nice, add assoc  pages to navbar links

---
+++ src/pages/Messages/Messages.jsx
@@ -0,0 +1,14 @@
+// import './Messages.css';
+
+const Messages = (props) => {
+	
+	// const {  } = props;
+
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default Messages;
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+// import './Profile.css';
+
+const Profile = (props) => {
+	
+	// const {  } = props;
+
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default Profile;
