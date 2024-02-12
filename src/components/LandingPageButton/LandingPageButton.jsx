0be2119827b4c26aaac1c41ffc3d4038ee61f78b
laurenmarsh <laurenmdb@gmail.com>
2024-02-12 15:59:51 -0500

    created landing page

---
+++ src/components/LandingPageButton/LandingPageButton.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '135vh', 
+    width: '100vw', 
+    position: 'absolute', 
+    top: 0,
+    left: 0,
+  },
+  button: {
+    cursor: 'pointer',
+    width: '334px',
+    height: '49px',
+    padding: '0px 8px',
+    border: '0',
+    boxSizing: 'border-box',
+    borderRadius: '8px',
+    boxShadow: '0px 0px 10px rgba(0,0,0,0.1)',
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    fontSize: '19px',
+    fontFamily: 'Roboto',
+    lineHeight: '24px',
+    outline: 'none',
+  },
+};
+
+const defaultProps = {
+  label: 'Roll Out',
+};
+
+const Button = (props) => {
+const navigate = useNavigate();
+
+const handleClick = () => {
+    navigate('/user/login'); 
+  };
+
+  return (
+    <div style={styles.container}> 
+      <button style={styles.button} onClick={handleClick}>
+        {props.label ?? defaultProps.label}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
