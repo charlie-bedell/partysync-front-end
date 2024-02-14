a67134e5a69ae7972738b1bc8dbeaa1d3de5a2c4
Ade <https://github.com/AdeShennaike>
2024-02-14 18:12:44 -0500

    got data to show

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,26 @@
 // import './Profile.css';
-import Image from '../../components/HomeImage/HomeImage';
+import Image from '../../components/HomeImage/HomeImage'
+import { getProfile } from '../../services/profile.js'
+import { useState, useEffect } from 'react';
 
 const Profile = (props) => {
+	const [profileDetails, setProfileDetails] = useState({});
 
-	// const {  } = props;
+	useEffect(() => {
+		const fetchProfiledata = async () => {
+			const profiledata = await getProfile();
+			setProfileDetails(profiledata);
+		};
+
+		fetchProfiledata();
+		console.log('profileDetails', profileDetails.user);
+	}, []);
 
 	return (
 		<div>
 			<Image />
+
+			
 		</div>
 	);
 };
