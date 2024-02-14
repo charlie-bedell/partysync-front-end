2d9cfcb36c1e56ef5842c9ed7d766a47d8323808
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-02-14 15:42:19 -0500

    simplify axios function to point to heroku only

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -6,18 +6,10 @@ const getToken = () => {
     resolve(token ? `Bearer ${token}` : null);
   });
 };
-// const api = axios.create({
-//   baseURL:
-//     process.env.NODE_ENV === "production"
-//       ? "https://partysync-5fbbda6a7756.herokuapp.com"
-//       : "http://localhost:3000",
-// });
-
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? import.meta.env.VITE_API_BASE_URL
-    : "http://localhost:3000",
+  baseURL:"https://partysync-5fbbda6a7756.herokuapp.com"
 });
+
 
 api.interceptors.request.use(
   async function (config) {
