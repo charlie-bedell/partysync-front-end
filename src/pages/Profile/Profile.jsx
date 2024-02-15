6ba8fa9328d5958c94e32e725a4797ac8fab9fff
laurenmarsh <laurenmdb@gmail.com>
2024-02-15 09:22:58 -0500

    profile details css

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,24 +2,31 @@
 import Image from '../../components/HomeImage/HomeImage'
 import { getProfile } from '../../services/profile.js'
 import { useState, useEffect } from 'react';
+import ProfileDetails from '../../components/ProfileDetails/ProfileDetails.jsx';
 
 const Profile = (props) => {
 	const [profileDetails, setProfileDetails] = useState({});
 
 	useEffect(() => {
 		const fetchProfiledata = async () => {
-			const profiledata = await getProfile();
-			setProfileDetails(profiledata);
+			try {
+				const profiledata = await getProfile(); 
+				console.log(profiledata); 
+				setProfileDetails(profiledata.user); 
+			} catch (error) {
+				console.error('Error fetching profile data:', error);
+			}
 		};
-
+	
 		fetchProfiledata();
-		console.log('profileDetails', profileDetails.user);
 	}, []);
+	
+	
 
 	return (
 		<div>
 			<Image />
-
+			<ProfileDetails profile={profileDetails} />
 			
 		</div>
 	);
